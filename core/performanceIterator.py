18ece60fde47efc18f959b2f20b5fba2ccae5bfb
udthegooner <udhbhav25@gmail.com>
2026-02-06 15:50:51 -0600

    performance Iterator tweaks

--- core/performanceIterator.py
+++ core/performanceIterator.py
@@ -1,10 +1,11 @@
 import logging
 import time
 from collections.abc import Iterable
+import torch
 
 class PerformanceIterator:
     def __init__(self, data_loader, checkpoint_dir, load_checkpoint_func,
-            save_checkpoint_func, log_file):
+            save_checkpoint_func, log_file, onGPU=False):
         if not isinstance(data_loader, Iterable):
             raise ValueError('Data is of uniterable '
                              'type %s' % (type(data_loader)))
@@ -14,13 +15,17 @@ class PerformanceIterator:
         self._save_checkpoint_func = save_checkpoint_func
         self._checkpoint_dir = checkpoint_dir
         self._log_file = log_file
+        self._onGPU = onGPU
+
         self._logger = logging.getLogger()
         logging.basicConfig(filename=log_file, encoding='utf-8', level=logging.DEBUG, filemode='w')
+        
         self._steps = 0
         self._duration = 0
         self._done = False
-        self._write_info()
         self._prev_time = None
+
+        self._logger.info('[{0}][{1}][][]'.format(time.time(),'INIT'))
 
     def __del__(self):
         self.complete()
@@ -29,23 +34,29 @@ class PerformanceIterator:
         self._iterator = iter(self._data_loader)
         return self
 
-    def __next__(self):
-        # Update the elapsed time.
-        cur_time = time.time()
-        if self._prev_time is None:
-            self._prev_time = cur_time
-        elapsed_time = cur_time - self._prev_time
-        self._duration += elapsed_time
-        self._prev_time = cur_time
+    def _record_step(self):
+        """Helper to synchronize GPU and log elapsed compute time."""
+        if self._onGPU:
+            torch.cuda.synchronize()
 
-        try:
-            val = next(self._iterator)
+        if self._prev_time is not None:
+            elapsed_time = time.time() - self._prev_time
+            self._duration += elapsed_time
             self._steps += 1
             self._write_info()
+
+    def __next__(self):
+        self._record_step()
+        try:
+            val = next(self._iterator)
         except StopIteration as e:
-            self._write_info()
+            self._done = True
+            self.complete()
             raise StopIteration
 
+        # Capture the time when the model STARTS its next compute loop, 
+        # which is right after the iterator returns the next batch.
+        self._prev_time = time.time()
         return val
 
     def __len__(self):
@@ -56,13 +67,11 @@ class PerformanceIterator:
         return self._done
 
     def complete(self):
-        self._done = True
-        self._write_info()
-        self._logger.info('[{0}][{1}][{2}][]'.format(time.time(),'PROGRESS',
-            'COMPLETE'))
+        if not self._done:
+            self._record_step()
+            self._done = True
+        self._logger.info('[{0}][{1}][{2}][{3}]'.format(time.time(),'COMPLETE','STEPS', self._steps))
 
     def _write_info(self):
-        self._logger.info('[{0}][{1}][{2}][{3}]'.format(time.time(),'PROGRESS',
-            'STEPS', self._steps))
-        self._logger.info('[{0}][{1}][{2}][{3}]'.format(time.time(),'PROGRESS',
-            'DURATION', self._duration))
+        self._logger.info('[{0}][{1}][{2}][{3}]'.format(time.time(),'PROGRESS','STEPS', self._steps))
+        self._logger.info('[{0}][{1}][{2}][{3}]'.format(time.time(),'PROGRESS','DURATION', self._duration))
